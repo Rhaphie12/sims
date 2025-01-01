7371853167ab8782b5c3d2117f1e113bb1fe1a3a
Rhaphie Costante <117087910+Rhaphie12@users.noreply.github.com>
2025-01-01 10:53:45 +0800

    Customized Message Boxes

---
+++ Messages Boxes/Staff Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sims.Messages_Boxes
+{
+    public partial class Staff_Login : Form
+    {
+        public Staff_Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
