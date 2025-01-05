9ca110b866f21d898489fb68a883e84f98e5047c
Rhaphie Costante <117087910+Rhaphie12@users.noreply.github.com>
2025-01-05 09:10:46 +0800

    modified

--- Dashboard Owner.Designer.cs
+++ Dashboard Owner.Designer.cs
@@ -44,7 +44,8 @@
             this.Label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.gunaCirclePictureBox1 = new Guna.UI.WinForms.GunaCirclePictureBox();
+            this.notifPic = new Guna.UI.WinForms.GunaPictureBox();
+            this.profilePicBox = new Bunifu.UI.WinForms.BunifuPictureBox();
             this.panel5 = new System.Windows.Forms.Panel();
             this.Label3 = new System.Windows.Forms.Label();
             this.Label2 = new System.Windows.Forms.Label();
@@ -55,7 +56,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.gunaCirclePictureBox2)).BeginInit();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gunaCirclePictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.notifPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profilePicBox)).BeginInit();
             this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -117,7 +119,7 @@
             this.SalesReportBtn.Padding = new System.Windows.Forms.Padding(35, 0, 20, 0);
             this.SalesReportBtn.Size = new System.Drawing.Size(299, 58);
             this.SalesReportBtn.TabIndex = 31;
-            this.SalesReportBtn.Text = "Sales  Report";
+            this.SalesReportBtn.Text = "Sales Report\r\n";
             this.SalesReportBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.SalesReportBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.SalesReportBtn.UseVisualStyleBackColor = true;
@@ -248,6 +250,7 @@
             this.SignoutBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.SignoutBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.SignoutBtn.UseVisualStyleBackColor = true;
+            this.SignoutBtn.Click += new System.EventHandler(this.SignoutBtn_Click);
             // 
             // CategoriesBtn
             // 
@@ -340,30 +343,48 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(299, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1194, 79);
+            this.panel2.Size = new System.Drawing.Size(1145, 79);
             this.panel2.TabIndex = 91;
             // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.gunaCirclePictureBox1);
+            this.panel3.Controls.Add(this.notifPic);
+            this.panel3.Controls.Add(this.profilePicBox);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel3.Location = new System.Drawing.Point(1108, 0);
+            this.panel3.Location = new System.Drawing.Point(1004, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(86, 79);
+            this.panel3.Size = new System.Drawing.Size(141, 79);
             this.panel3.TabIndex = 3;
             // 
-            // gunaCirclePictureBox1
+            // notifPic
             // 
-            this.gunaCirclePictureBox1.BaseColor = System.Drawing.Color.White;
-            this.gunaCirclePictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.gunaCirclePictureBox1.Image = global::sims.Properties.Resources.account;
-            this.gunaCirclePictureBox1.Location = new System.Drawing.Point(11, 6);
-            this.gunaCirclePictureBox1.Name = "gunaCirclePictureBox1";
-            this.gunaCirclePictureBox1.Size = new System.Drawing.Size(65, 65);
-            this.gunaCirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.gunaCirclePictureBox1.TabIndex = 92;
-            this.gunaCirclePictureBox1.TabStop = false;
-            this.gunaCirclePictureBox1.UseTransfarantBackground = false;
+            this.notifPic.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.notifPic.BaseColor = System.Drawing.Color.White;
+            this.notifPic.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.notifPic.Image = global::sims.Properties.Resources.bell__1_;
+            this.notifPic.Location = new System.Drawing.Point(59, 12);
+            this.notifPic.Name = "notifPic";
+            this.notifPic.Size = new System.Drawing.Size(26, 38);
+            this.notifPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.notifPic.TabIndex = 12;
+            this.notifPic.TabStop = false;
+            // 
+            // profilePicBox
+            // 
+            this.profilePicBox.AllowFocused = false;
+            this.profilePicBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.profilePicBox.AutoSizeHeight = true;
+            this.profilePicBox.BorderRadius = 19;
+            this.profilePicBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.profilePicBox.Image = global::sims.Properties.Resources.account;
+            this.profilePicBox.IsCircle = true;
+            this.profilePicBox.Location = new System.Drawing.Point(91, 12);
+            this.profilePicBox.Name = "profilePicBox";
+            this.profilePicBox.Size = new System.Drawing.Size(38, 38);
+            this.profilePicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.profilePicBox.TabIndex = 13;
+            this.profilePicBox.TabStop = false;
+            this.profilePicBox.Type = Bunifu.UI.WinForms.BunifuPictureBox.Types.Circle;
             // 
             // panel5
             // 
@@ -402,7 +423,7 @@
             this.DashboardPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.DashboardPanel.Location = new System.Drawing.Point(299, 79);
             this.DashboardPanel.Name = "DashboardPanel";
-            this.DashboardPanel.Size = new System.Drawing.Size(1194, 700);
+            this.DashboardPanel.Size = new System.Drawing.Size(1145, 700);
             this.DashboardPanel.TabIndex = 92;
             // 
             // DashboardOwner
@@ -410,7 +431,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(196)))), ((int)(((byte)(125)))));
-            this.ClientSize = new System.Drawing.Size(1493, 779);
+            this.ClientSize = new System.Drawing.Size(1444, 779);
             this.Controls.Add(this.DashboardPanel);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.PanelMenu);
@@ -418,6 +439,7 @@
             this.Name = "DashboardOwner";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main Dashboard (Owner)";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.PanelMenu.ResumeLayout(false);
             this.InventoryPanelSubMenu.ResumeLayout(false);
             this.panel12.ResumeLayout(false);
@@ -425,7 +447,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.gunaCirclePictureBox2)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.gunaCirclePictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.notifPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profilePicBox)).EndInit();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
             this.ResumeLayout(false);
@@ -445,7 +468,6 @@
         private System.Windows.Forms.Panel panel5;
         internal System.Windows.Forms.Label Label3;
         internal System.Windows.Forms.Label Label2;
-        private Guna.UI.WinForms.GunaCirclePictureBox gunaCirclePictureBox1;
         private Guna.UI.WinForms.GunaCirclePictureBox gunaCirclePictureBox2;
         private System.Windows.Forms.Panel DashboardPanel;
         internal FontAwesome.Sharp.IconButton inventoryBtn;
@@ -455,6 +477,8 @@
         internal FontAwesome.Sharp.IconButton UserBtn;
         internal FontAwesome.Sharp.IconButton SalesReportBtn;
         internal FontAwesome.Sharp.IconButton SalesBtn;
+        internal Guna.UI.WinForms.GunaPictureBox notifPic;
+        internal Bunifu.UI.WinForms.BunifuPictureBox profilePicBox;
     }
 }
 
--- Dashboard Owner.cs
+++ Dashboard Owner.cs
@@ -1,7 +1,13 @@
 ﻿using FontAwesome.Sharp;
 using Guna.UI.WinForms;
+using MySql.Data.MySqlClient;
 using sims.Admin_Side;
 using sims.Admin_Side.Category;
+using sims.Admin_Side.Items;
+using sims.Admin_Side.Sales;
+using sims.Admin_Side.Sales_Report;
+using sims.Admin_Side.Stocks;
+using sims.Admin_Side.Users;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -22,12 +28,44 @@ namespace sims
         public DashboardOwner()
         {
             InitializeComponent();
+            ShowUsernameWithGreeting();
             customizeDesign();
             leftBorderBtn = new GunaPanel
             {
                 Size = new Size(10, 58)
             };
             PanelMenu.Controls.Add(leftBorderBtn);
+        }
+        private void ShowUsernameWithGreeting()
+        {
+            dbModule db = new dbModule();
+            string query = "SELECT username FROM users LIMIT 1";
+
+            using (MySqlConnection conn = db.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        object result = cmd.ExecuteScalar();
+
+                        if (result != null)
+                        {
+                            string username = result.ToString();
+                            Label2.Text = $"HI! {username},";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
         }
         private void customizeDesign()
         {
@@ -130,31 +168,50 @@ namespace sims
         private void ItemsBtn_Click(object sender, EventArgs e)
         {
             ActivateButton(sender, Color.FromArgb(255, 255, 255));
+            OpeninPanel(new Manage_Items());
 
         }
 
         private void StocksBtn_Click(object sender, EventArgs e)
         {
             ActivateButton(sender, Color.FromArgb(255, 255, 255));
+            OpeninPanel(new Manage_Stock());
 
         }
 
         private void SalesBtn_Click(object sender, EventArgs e)
         {
             ActivateButton(sender, Color.FromArgb(255, 255, 255));
+            OpeninPanel(new Manage_Sales());
 
         }
 
         private void SalesReportBtn_Click(object sender, EventArgs e)
         {
             ActivateButton(sender, Color.FromArgb(255, 255, 255));
+            OpeninPanel(new Manage_Sales_Report());
 
         }
 
         private void UserBtn_Click(object sender, EventArgs e)
         {
             ActivateButton(sender, Color.FromArgb(255, 255, 255));
+            OpeninPanel(new Manage_User_Staff());
+        }
 
+        private void SignoutBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to logout?",
+                "Logout?",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Warning
+            );
+            if (result == DialogResult.OK)
+            {
+                this.Hide();
+                new Login_Form().Show();
+            }
         }
     }
 }
